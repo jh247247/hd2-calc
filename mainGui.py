6bcd629de9eb0631ef528adc5ddb3119529cdf69
Jack Hosemans <jh247247@gmail.com>
2013-07-06 12:44:12 +1000

    Added some docstrings and why was ElementHandler a QScrollArea?

--- mainGui.py
+++ mainGui.py
@@ -155,26 +155,40 @@ class StatusHandler:
 
 # Everything has to start somewhere. This app starts here.
 
-class ElementHandler(QtGui.QScrollArea):
+class ElementHandler(QtGui.QWidget):
     def __init__(self, parent=None):
+        """
+        Creates a list for storing the individual gui elements
+        Might need restructuring later down the line for the math stuff,
+        but this will do for now.
+        """
         super(ElementHandler,self).__init__()
         self.elements = []
 
+        # Create a vertical layout so we can scroll.
         self.elementLayout = QtGui.QVBoxLayout(self)
         self.setLayout(self.elementLayout)
         self.elementLayout.setAlignment(QtCore.Qt.AlignBottom)
 
         self.appendElement()
-        self.appendElement()
 
     def appendElement(self):
+        """
+        Appends a new element and finalizes the previous one.
+        """
         newElement = guiMathElement(self)
         self.elementLayout.addWidget(newElement)
+        # Finalize the previous element so wierd stuff doesn't happen.
         if len(self.elements) != 0:
             self.elements[-1].finalize()
         self.elements.append(newElement)
 
     def resizeEvent(self,event):
+        """
+        If this is resized, ensure that the inner widgets are properly
+        resized as well.
+        """
+
         super(ElementHandler, self).resizeEvent(event)
 
         self.resize(event.size())
@@ -187,16 +201,27 @@ class ElementHandler(QtGui.QScrollArea):
 
 
 class guiMathElement(QtGui.QWidget):
+    # This should be about 1-2 lines for our text size.
     INITIAL_HEIGHT = 66
     def __init__(self, parent=None):
+        """
+        Init stuff to be used in this gui element.
+        """
         super(guiMathElement,self).__init__(parent)
         self.__initChildren()
-
+        self.finalize()
 
     def __initChildren(self):
+        """
+        Inits child widgets.
+        self.text is the textbox.
+        """
         self.text = QtGui.QTextEdit()
         # Magic number comes from added gui elements and text.
         self.text.setFixedHeight(self.INITIAL_HEIGHT)
+
+        # connect textchanges signal to our own private slot.
+        # This resizes the textBox as new text is added.
         self.text.textChanged.connect(self.__textChanged)
         self.text.setSizePolicy(QtGui.QSizePolicy.Expanding, \
                                 QtGui.QSizePolicy.Fixed)
@@ -207,6 +232,11 @@ class guiMathElement(QtGui.QWidget):
         self.setLayout(self.layout)
 
     def __textChanged(self):
+        """
+        This function is only called by the signal textChanged from the self.text
+        textbox. It resizes the textbox and the containing widget to make sure there are no
+        scroll bars as they are terrible on a touchscreen.
+        """
         # base resizing of the widget on the changing text.
         # A sprinkling of magic numbers to keep things nice looking.
         baseHeight = self.text.document().size().height()
@@ -217,12 +247,14 @@ class guiMathElement(QtGui.QWidget):
         self.text.ensureCursorVisible()
 
 
-    def resizeEvent(self,event):
-        super(guiMathElement,self).resizeEvent(event)
     def finalize(self):
+        """
+        This finalizes the code in the text box.
+        Should only be called once the code is rendered.
+        Also greys out the box so that text cannot be edited.
+        """
         self.text.setReadOnly(True)
-
-
+        self.text.setEnabled(False)
 
 def main():
     app = QtGui.QApplication(sys.argv)
